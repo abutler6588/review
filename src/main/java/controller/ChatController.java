4a645f0ec132e3f1c0c1b32f97a556815d2ce6ab
abutler6588 <abutler6588@gmail.com>
2021-03-17 14:40:47 -0500

    first commit

---
+++ src/main/java/controller/ChatController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.ChatForm;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.MessageService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    private MessageService messageService;
+
+    public ChatController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public String getChatPage(ChatForm chatForm, Model model) {
+        model.addAttribute("chatMessages", this.messageService.getChatMessages());
+        return "chat";
+    }
+
+    @PostMapping
+    public String postChatMessage(ChatForm chatForm, Model model) {
+        this.messageService.addMessage(chatForm);
+        chatForm.setMessageText("");
+        model.addAttribute("chatMessages", this.messageService.getChatMessages());
+        return "chat";
+    }
+
+    @ModelAttribute("allMessageTypes")
+    public String[] allMessageTypes () {
+        return new String[] { "Say", "Shout", "Whisper" };
+    }
+
+}
